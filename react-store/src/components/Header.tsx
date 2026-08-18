49e3f3d30818ff05d744ce0cdeadcda35f153b50
nhuquynh-git <24521515@gm.uit.edu.vn>
2026-08-18 11:17:59 +0700

    Done Summary 1, 2, 3

--- react-store/src/components/Header.tsx
+++ react-store/src/components/Header.tsx
@@ -2,24 +2,16 @@ import type React from "react";
 import { ThemeName, User } from "../types";
 import { LoginBar } from "./LoginBar";
 import { ThemeToggle } from "./ThemeToggle";
+import { useUserTheme } from "../state/UserThemeContext";
 
 interface HeaderProps {
   cartCount: number;
-  user: User | null;
-  theme: ThemeName;
-  onToggleTheme: () => void;
-  onLogin: () => void;
-  onLogout: () => void;
 }
 
 export const Header = ({
   cartCount,
-  user,
-  theme,
-  onToggleTheme,
-  onLogin,
-  onLogout,
 }: HeaderProps): React.JSX.Element => {
+  const { user, theme, login : onLogin, logout : onLogout, toggleTheme : onToggleTheme } = useUserTheme();
   return (
     <header className="store-header">
       <h1 className="store-branch">React Store</h1>
--- react-store/src/state/UserThemeContext.tsx
+++ react-store/src/state/UserThemeContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react";
-import type React from "react";
+import React from "react";
 import type { ThemeName, User } from "../types";
 
 export interface UserThemeContextValue {
@@ -20,12 +20,34 @@ interface UserThemeProviderProps {
 export const UserThemeProvider = ({
   children,
 }: UserThemeProviderProps): React.JSX.Element => {
+  const [user, setUser] = React.useState<User | null>(null);
+  const [theme, setTheme] = React.useState<ThemeName>("light");
+
+  const login = (): void => {
+    setUser({ name: "John Doe" });
+  }
+
+  const logout = (): void => {
+    setUser(null);
+  }
+
+  const toggleTheme = (): void => {
+    setTheme((prev) => prev === "light" ? "dark" : "light");
+  }
+
+  const sharedValue: UserThemeContextValue = {
+    user,
+    theme,
+    login,
+    logout,
+    toggleTheme
+  }
   return (
-    <UserThemeContext.Provider value={null}>
+    <UserThemeContext.Provider value={sharedValue}>
       {children}
     </UserThemeContext.Provider>
   );
-};
+};  
 
 export const useUserTheme = (): UserThemeContextValue => {
   const context = useContext(UserThemeContext);
